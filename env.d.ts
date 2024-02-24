f6b3a483bb71312f7f6d911c2dbf21b87d904e17
Type-32 <wilsonsu2008@hotmail.com>
2024-02-25 01:21:49 +0800

    Man, I hate Vue + Vite

--- env.d.ts
+++ env.d.ts
@@ -1,13 +1,9 @@
 /// <reference types="vite/client" />
-declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            VITE_CMS_URL: string;
-            VITE_CMS_TOKEN: string;
-        }
-    }
+interface ImportMetaEnv {
+    readonly VITE_CMS_URL: string;
+    readonly VITE_CMS_TOKEN: string;
 }
 
-// If this file has no import/export statements (i.e. is a script)
-// convert it into a module by adding an empty export statement.
-export {}
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
